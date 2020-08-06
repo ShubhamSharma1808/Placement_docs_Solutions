38ff68d3844b7d1210cabda6b13c193ecb066548
SHUBHAM SHARMA <48373880+ShubhamSharma1808@users.noreply.github.com>
2020-08-06 18:34:44 +0530

    Update Q1.cpp

--- Sprinkler/Q1.cpp
+++ Sprinkler/Q1.cpp
@@ -2,5 +2,4 @@
  Queue se solve ho jaega queue me push krte rhenge 
   jaise me (0,0) pe hu to Queue me (0+arr[0][0],0) and (0,arr[0][0]+0) push krdenge and value ek se bada denge.
   agr up down left right bhi hota to bhi yhi approach use kr skte hai.
-  
- */
+*/
