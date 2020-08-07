109df4e8057971c0524815c9cfffa70eebab1aa8
SHUBHAM SHARMA <48373880+ShubhamSharma1808@users.noreply.github.com>
2020-08-07 14:57:53 +0530

    Create Emp. loger basic.cpp

---
+++ Cisco/Emp. loger basic.cpp
@@ -0,0 +1,11 @@
+
+// input ko aise scan krke easy hojaega :  
+    scanf("%c,%d:%d:%d,%d:%d:%d",&name,&entry_hr,&entry_min,&entry_sec,&exit_hr,&exit_min,&exit_sec);
+    
+/*
+agr multiple query ati hai to hrr ek time pe calculate krna better hoga
+agr ek query ati hai to frk ni pdega.
+and,
+    Shivam Maurya IITG: aur maximum emp at certain time puchha hta to,
+    Shivam Maurya IITG: on entry cnt++, exit cnt--, update globalcnt using max(cnt, globalcnt)
+*/
