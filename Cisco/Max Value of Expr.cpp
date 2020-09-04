a916532f6bf6cf00bd5aadae59763e9e90f0faec
SHUBHAM SHARMA <48373880+ShubhamSharma1808@users.noreply.github.com>
2020-09-04 11:53:48 +0530

    added updated_solve

--- Cisco/Max Value of Expr.cpp
+++ Cisco/Max Value of Expr.cpp
@@ -1,7 +1,8 @@
 /*
  * Dp variation of matrix chain multiplication.
  * it cant handle mulitple digit value test case , for that we need diff. implemetation like we will need to have two diff. arrays storing values 
- * of operands and operators. this is the one case. one more issue is that we just cant take local max to find the global max
+ * of operands and operators. this is the one case. one more issue is that we just cant take local max to find the global max we can get global max from local min 
+ * so we need to store min and max both to calculate global max, same goes for global min also.
  */
 #include<bits/stdc++.h>
 using namespace std;
@@ -68,13 +69,68 @@ T lcm(T a, T b){	return a/gcd(a, b) * b;		}
    return mx;
  }
  
+
+int updated_solve(vector<vii>& cache, vi& oprnd, vector<char>& oprn, int i, int j){
+     if(cache[i][j].first!=-1)
+         return cache[i][j];     
+     if(i==j){        
+        printf("value returned from [ %d , %d ] is {%d,%d}\n",i,j,oprnd[i],oprnd[j]);
+        cache[i][j] = {oprnd[i],oprnd[i]}; 
+        return cache[i][j];
+     }
+   ii tmp1,tmp2;
+   int mn = INT_MIN, mx = INT_MIN; 
+   rep(k,i,j){
+     tmp1 = solve(cache,str,i,k);
+     tmp2 = solve(cache,str,k+1,j); 
+     //cout<<tmp1<<" "<<tmp2<<endl;
+		 int a0=tmp1.first,a1=tmp1.second,b0=tmp2.first,b1=tmp2.second;
+     if(oprn[k]=='+'){
+			t1 = max(a0+b0,max(a0+b1,max(a1+b0,a1+b1)));
+			t2 = min(a0+b0,min(a0+b1,min(a1+b0,a1+b1)));
+      mx = max(mx,t1);
+			mn = min(mn,t2);
+		 }
+		 else  if(oprn[k]=='-'){
+			t1 = max(a0-b0,max(a0-b1,max(a1-b0,a1-b1)));
+			t2 = min(a0-b0,min(a0-b1,min(a1-b0,a1-b1)));
+      mx = max(mx,t1);
+			mn = min(mn,t2);
+		 }
+     else{
+			t1 = max(a0*b0,max(a0*b1,max(a1*b0,a1*b1)));
+			t2 = min(a0*b0,min(a0*b1,min(a1*b0,a1*b1)));
+      mx = max(mx,t1);
+			mn = min(mn,t2);
+		 }     
+   }
+   printf("value returned from [ %d , %d ] is {%d,%d}\n",i,j,mx,mn);
+   cache[i][j]={mx,mn};  
+   return cache[i][j];
+}
 int main()
 {
 	fastio;
   string str;
   cin>>str;
-  vector<vi> cache(str.size(),vi(str.size(),-1));
-  cout<<solve(cache,str,0,str.size()-1);
+  vector<vii> cache(sz(str),vi(sz(str),{-1,-1}));
+  vi oprnd;
+  vector<char> oprn;
+	int tmp=0,count=1;
+	rep(i,0,sz(str)){
+		if(str[i]>='0' && str[i]<'9'){
+			tmp = tmp*count + (str[i]-'0');
+			count *=10;
+		}
+		else{
+			oprnd.eb(tmp);
+			tmp=0;
+			count=1;
+			oprn.eb(str[i]);
+		}
+	}	
+  //cout<<solve(cache,str,0,str.size()-1);	
+	cout<<solve(cache,oprnd,oprn,0,sz(oprnd)-1);
   return 0;
 }
  
